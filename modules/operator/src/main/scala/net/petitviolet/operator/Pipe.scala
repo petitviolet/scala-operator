10d2d65ea6ea2fe173d56deacabf21e173abbca4
petitviolet <violethero0820@gmail.com>
2017-07-26 13:29:55 +0900

    andThen alias for `|>`

--- modules/operator/src/main/scala/net/petitviolet/operator/Pipe.scala
+++ modules/operator/src/main/scala/net/petitviolet/operator/Pipe.scala
@@ -1,6 +1,7 @@
 package net.petitviolet.operator
 
 final class Pipe[A] private[operator](val value: A) extends AnyVal {
-  def |>[B](f: A => B): B = f(value)
-  def |>>(f: A => Unit): A = { f(value); value }
+  final def |>[B](f: A => B): B = andThen(f)
+  final def andThen[B](f: A => B): B = f(value)
+  final def |>>(f: A => Unit): A = { f(value); value }
 }
