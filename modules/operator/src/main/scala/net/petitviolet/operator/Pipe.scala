2355315df4f4a3fda58c0cbd6f4bfca65d69cb11
petitviolet <mail@petitviolet.net>
2018-11-07 18:54:18 +0900

    added pipe and tap

--- modules/operator/src/main/scala/net/petitviolet/operator/Pipe.scala
+++ modules/operator/src/main/scala/net/petitviolet/operator/Pipe.scala
@@ -2,7 +2,9 @@ package net.petitviolet.operator
 
 final class Pipe[A] private[operator](val value: A) extends AnyVal { self =>
   def |>[B](f: A => B): B = andThen(f)
+  def pipe[B](f: A => B): B = andThen(f)
   def andThen[B](f: A => B): B = f(value)
-  def <|(f: A => Unit): A = { f(value); value }
+  def <|(f: A => Unit): A = tap(f)
+  def tap(f: A => Unit): A = { f(value); value }
   def |>>(f: A => Unit): A = self <| f
 }
